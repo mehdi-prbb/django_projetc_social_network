3c42c4129341d76658494ea161d3b7e3a666db92
mehdi <mpourbaba@gmail.com>
2023-01-04 22:38:20 +0330

    implelment password validation to form

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,7 +6,8 @@ from django.core.exceptions import ValidationError
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label='confirm password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
     def clean_email(self):
         email = self.cleaned_data['email']
@@ -23,3 +24,14 @@ class UserRegistrationForm(forms.Form):
         if user:
             raise ValidationError('This username Already Exists')
         return username
+
+    def clean(self):
+        cd = super().clean()
+        pass1 = cd.get('password1')
+        pass2 = cd.get('password2')
+
+        if pass1 and pass2 and len(pass1) <= 8:
+            raise ValidationError("Password must be more than 8 character")
+
+        if pass1 != pass2:
+            raise ValidationError("Password Does'nt match together")
